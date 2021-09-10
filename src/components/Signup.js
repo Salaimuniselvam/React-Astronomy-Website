cdcd098d84c685e2e97ed2445e596cc6759792f4
Salaimuniselvam <salaimuniselvam7@gmail.com>
2021-09-10 21:08:19 +0530

    react website

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,7 +11,6 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Navbar from "./Navbar";
-import { Route } from "react-router-dom";
 import axios from "axios";
 import MainPage from "./MainPage";
 
